adb266cc9cefc13052a09f81815eea2146124dc2
YazidHabibi <yazid2505@student.abudzar.sch.id>
2025-06-05 10:53:04 +0700

    commit Terahkir

---
+++ vite-project/src/SAS.jsx
@@ -0,0 +1,10 @@
+export default function SAS() {
+    return (<>
+        <title>PRAKTEK IT SAS</title>
+
+        <h1>Ini Adalah Tugas Praktek IT SAS</h1>
+    <img src="lebih gelo cuct.png" alt="" />
+    <p>Dan ini adalah saya ketika saya masih berada di kelas 10 😎</p>
+    </>   
+    )
+}
